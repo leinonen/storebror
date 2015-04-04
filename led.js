809df7404ea8f0ed846d9994a86adecc0e0340e2
leinonen <peter@leinonen.se>
2015-04-04 09:58:42 +0000

    led stuff

--- led.js
+++ led.js
@@ -1,12 +1,14 @@
 var GPIO = require('onoff').Gpio;
-var led = new GPIO(18, 'out');
+var led = new GPIO(17, 'out');
 
 var status = true;
 
 function ledStuff(){
 	if (status){
+		console.log('led on');
 		led.writeSync(1);
 	} else {
+		console.log('led off');
 		led.writeSync(0);
 	}
 	status = !status;
