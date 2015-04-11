37227b12f3ae10336c0f94ca0b36124a27434b74
Peter Leinonen <peter@leinonen.se>
2015-04-11 22:07:53 +0200

    timer

--- slave.js
+++ slave.js
@@ -1,18 +1,14 @@
 var ReportClient = require('./report-client');
 
 var client = new ReportClient();
-
 var timer;
 
-function startTimer() {
-	timer = setInterval(function(){
-		client.report(client_id);
-	}, 10000);
-}
 
 client.on('connected', function(client_id) {
 	client.report(client_id);
-	startTimer();
+	timer = setInterval(function(){
+		client.report(client_id);
+	}, 10000);
 });
 
 client.on('report.sent', function(response) {
