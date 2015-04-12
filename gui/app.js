fff640b3c5a6bd8307357df03dfabfde00847955
Peter Leinonen <peter@leinonen.se>
2015-04-12 11:59:14 +0200

    gui stuff

--- gui/app.js
+++ gui/app.js
@@ -25,6 +25,18 @@ app.controller('AppController', function($http,$interval){
 
 		$http.get('/stats').then(function(response){
 			vm.total = response.data;
+			if (vm.total.size.unit === 'G' && vm.total.size.value > 1000.0){
+				vm.total.size.unit = 'T';
+				vm.total.size.value /= 1000.0;
+			}
+			if (vm.total.used.unit === 'G' && vm.total.used.value > 1000.0){
+				vm.total.used.unit = 'T';
+				vm.total.used.value /= 1000.0;
+			}
+			if (vm.total.avail.unit === 'G' && vm.total.avail.value > 1000.0){
+				vm.total.avail.unit = 'T';
+				vm.total.avail.value /= 1000.0;
+			}
 		});
 
 		$http.get('/config').then(function(response){
