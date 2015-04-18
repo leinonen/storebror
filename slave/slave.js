18b7471d99cf8204845f316226bae5281f5e90a9
Peter Leinonen <peter@leinonen.se>
2015-04-19 00:38:28 +0200

    bugfix

--- slave/slave.js
+++ slave/slave.js
@@ -2,7 +2,7 @@ var ReportClient = require('./report-client');
 var config = require('./config/slave-config');
 var client = new ReportClient();
 
-console.log('storebror client started. will send reports every %d ms.' + config.reportInterval);
+console.log('storebror client started. will send reports every %d ms.', config.reportInterval);
 
 client.on('report.sent', function (response) {
 	//console.log('report sent: ' + response.status);
